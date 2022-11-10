d3d6b5fed392394ea1aba2b5a036bb78c51b124c
PrachiBhoj <prachideore19@gmail.com>
2022-11-10 14:59:01 +0530

    new class

---
+++ src/main/java/com/example/yatra/hotel.java
@@ -0,0 +1,13 @@
+package com.example.yatra;
+
+
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class hotel {
+    @GetMapping("/myhotel")
+    public String getData() {
+        return "Please book your hhhh Flight ticket, Choose Your Fav Airlines Indigo";
+    }
+}
